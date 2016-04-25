c396aba2dd50aa3725b5d18a56126875cb23c47b
ddbr <daniel.braeker@gmail.com>
2016-04-25 16:46:05 +0200

    sidebar styles footer removed

--- footer.php
+++ footer.php
@@ -13,13 +13,7 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'p619' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'p619' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'p619' ), 'p619', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<!-- db:	footer removed -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
