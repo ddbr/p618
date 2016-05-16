d9b342709474451f467a98509c9efe2b636e7f77
ddbr <daniel.braeker@gmail.com>
2016-05-16 17:02:07 +0200

    jquery

--- functions.php
+++ functions.php
@@ -116,7 +116,7 @@ function p619_scripts() {
 
 	wp_enqueue_script( 'p619-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'p619-scrolling', get_template_directory_uri() . '/js/scroll.js', array(  ) );
+	wp_enqueue_script( 'p619-scrolling', get_template_directory_uri() . '/js/scroll.js', array( 'jquery' ), '20160516', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
