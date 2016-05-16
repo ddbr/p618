74a649ce080a22b38942f168042826c315966a54
ddbr <daniel.braeker@gmail.com>
2016-05-16 16:45:36 +0200

    js scrolling

--- js/scroll.js
+++ js/scroll.js
@@ -4,18 +4,24 @@
  * Handles scrolling events on the Page.
  */
 
+ // scrolling check Variable
  var didScroll = false;
 
+ // scrolling event
  window.onscroll = doThisStuffOnScroll;
-
  function doThisStuffOnScroll() {
      didScroll = true;
-     console.log('You scrolledxxxxx');
  }
 
+ // Scrolling check interval function 1000ms
  setInterval(function() {
      if(didScroll) {
          didScroll = false;
-         console.log('You scrolled');
+         $('#masthead').trigger('switch-header');
      }
- }, 100);
+ }, 1000);
+
+$('#masthead').bind('switch-header',function() {
+  //$('.site-header').addClass('fixed-header')
+  console.log('scrolling');
+});
