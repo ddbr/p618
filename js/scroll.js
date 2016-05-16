7198af8a9773359494b1c48e5d13a3868ee6db51
ddbr <daniel.braeker@gmail.com>
2016-05-16 18:21:11 +0200

    header fixing

--- js/scroll.js
+++ js/scroll.js
@@ -23,11 +23,8 @@
 
 $('#masthead').bind('switch-header',function() {
   if($( document ).scrollTop() > 45){
-    var y_off = $("#content").offset().top;
-    $("#content").css('margin-top', y_off );
     $( this ).addClass('fixed-header');
   } else {
     $( this ).removeClass('fixed-header');
-    $("#content").css('margin-top',0);
   }
 });
