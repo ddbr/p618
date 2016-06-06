88d364c5962187ed03b76e16fe9265e62a1cc9de
ddbr <daniel.braeker@gmail.com>
2016-06-06 21:37:32 +0200

    content portfoliio template

--- page-portfolio.php
+++ page-portfolio.php
@@ -5,8 +5,7 @@
  * @package p619
  */
 
-/*get_header(); */
-?>
+get_header();?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -14,7 +13,7 @@
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+				get_template_part( 'template-parts/content', 'portfolio' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
