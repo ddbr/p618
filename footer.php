f3d2b19025767d5c2b4350b50d1ccb5b0f21e905
ddbr <daniel.braeker@gmail.com>
2016-06-08 12:35:40 +0200

    af

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
  */
 $footer_text = get_theme_mod("footer_text", false);
 $url_pattern = "~[[:alpha:]]+://[^<>[:space:]]+[[:alnum:]/]~";
-$footer_text = preg_replace($mail_pattern, '<a href=\"\\0\">\\0</a>', $footer_text);
+$footer_text = preg_replace($url_pattern, '<a href=\"\\0\">\\0</a>', $footer_text);
 $mail_pattern = "/([A-z0-9_-]+\@[A-z0-9_-]+\.)([A-z0-9\_\-\.]{1,}[A-z])/";
 $footer_text = preg_replace($mail_pattern, '<a href="mailto:$1$2">$1$2</a>', $footer_text);
 
