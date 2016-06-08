9d1daff66b0673b202fa58ac9f606bbc9bd469b4
ddbr <daniel.braeker@gmail.com>
2016-06-08 10:40:29 +0200

    footer text

--- inc/customizer.php
+++ inc/customizer.php
@@ -24,7 +24,20 @@ function p619_customize_register( $wp_customize ) {
 				'settings'		=> 'corporate_logo',
 				'section'		=> 'title_tagline',
 				'label'			=> __( 'Corporate Logo', 'theme-slug' ),
-				'description'	=> __( 'Select the logo Image for your Page', 'theme-slug' )
+				'description'	=> __( 'Select the logo image for your page', 'theme-slug' )
+	) ) );
+
+	$wp_customize->add_setting( 'footer_text',
+		array(
+			'transport'		=> 'postMessage'
+	) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_text',
+			array(
+				'settings'		=> 'footer_text',
+				'section'		=> 'title_tagline',
+				'type' => 'text',
+				'label'			=> __( 'Footer Text', 'theme-slug' ),
+				'description'	=> __( 'Enter text for footer line', 'theme-slug' )
 	) ) );
 }
 add_action( 'customize_register', 'p619_customize_register' );
