040eddca490507cddb28ae3c5e09415ad9a26d01
ddbr <daniel.braeker@gmail.com>
2016-06-15 20:34:32 +0200

    menu toggle

--- header.php
+++ header.php
@@ -25,26 +25,27 @@
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'p619' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			$corporate_logo = get_theme_mod("corporate_logo", false);
-			if ( ! $corporate_logo ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo $corporate_logo ?>" id="corporate_logo"></a></h1>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', false );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'p619' ); ?></button>
+			<div class="site-branding">
+				<?php
+				$corporate_logo = get_theme_mod("corporate_logo", false);
+				if ( ! $corporate_logo ) : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo $corporate_logo ?>" id="corporate_logo"></a></h1>
+				<?php
+				endif;
+
+				$description = get_bloginfo( 'description', false );
+				if ( $description || is_customize_preview() ) : ?>
+					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+				<?php
+				endif; ?>
+			</div><!-- .site-branding -->
+
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="true"><?php esc_html_e( 'Primary Menu', 'p619' ); ?></button>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
