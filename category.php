cb279bea6fcef33e4ceb9b08c73bf5bb97feb024
ddbr <daniel.braeker@gmail.com>
2016-06-25 14:52:10 +0200

    f

--- category.php
+++ category.php
@@ -11,6 +11,29 @@ get_header(); ?>
 
 <!-- #content Starts -->
 <?php woo_content_before(); ?>
+
+<header class="entry-header">
+	<?php
+	if ( is_product_category() ){
+	    global $wp_query;
+
+	    // get the query object
+	    $cat = $wp_query->get_queried_object();
+
+	    // get the thumbnail id using the queried category term_id
+	    $thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true );
+
+	    // get the image URL
+	    $image = wp_get_attachment_url( $thumbnail_id );
+
+	    // print the IMG HTML
+	    echo "<img src='{$image}' alt='' width='762' height='365' />";
+	};
+	the_title( '<h1 class="entry-title">', '</h1>' );
+	?>
+	<div class="down-arrow"></div>
+</header><!-- .entry-header -->
+
 <div id="content" class="col-full">
 
 		<div id="main-sidebar-container">
