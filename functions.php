06b36e3ca1e50292bfcfd113100a1d0d6a4f1993
ddbr <daniel.braeker@gmail.com>
2016-06-25 10:54:49 +0200

    f

--- functions.php
+++ functions.php
@@ -75,10 +75,10 @@ function p619_setup() {
 	//add post-formats to post_type 'my_custom_post_type'
 	add_post_type_support( 'featured_product', 'post-formats' );
 
-/*
+
 	add_theme_support( 'post-formats', array(
 		'featured_product'
-	) );*/
+	) );
 
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'p619_custom_background_args', array(
