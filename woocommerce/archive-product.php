4a30283a328f77e6b02e899ffb2509d6b2b56e5c
ddbr <daniel.braeker@gmail.com>
2016-07-01 13:29:56 +0200

    f

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -81,7 +81,7 @@ get_header( 'shop' ); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php wc_get_template_part( 'content', 'single-product' ); //wc_get_template_part( 'content', 'product' ); ?>
+					<?php wc_get_template_part( 'content', 'product_details' ); //wc_get_template_part( 'content', 'product' ); ?>
 
 				<?php endwhile; // end of the loop. ?>
 
