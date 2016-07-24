78e46d53e45d304ddc09386b936846da401b5a10
ddbr <daniel.braeker@gmail.com>
2016-07-24 10:20:48 +0200

    f

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -26,18 +26,18 @@ global $post, $product;
 			$attachment_count = count( $product->get_gallery_attachment_ids() );
 			$gallery          = $attachment_count > 0 ? '[product-gallery]' : '';
 			$props            = wc_get_product_attachment_props( get_post_thumbnail_id(), $post );
-			$image            = get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ), array(
+			/*$image            = get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ), array(
 				'title'	 => $props['title'],
 				'alt'    => $props['alt'],
-			) );
+			) );*/
 			echo apply_filters(
 				'woocommerce_single_product_image_html',
 				sprintf(
-					'<a href="%s" itemprop="image" class="woocommerce-main-image zoom test" title="%s" data-rel="prettyPhoto%s">%s</a>',
+					'<a href="%s" itemprop="image" class="woocommerce-main-image zoom" title="%s" data-rel="prettyPhoto%s"><div style="background-image: url(%s)"></div></a>',
 					esc_url( $props['url'] ),
 					esc_attr( $props['caption'] ),
 					$gallery,
-					$image
+					esc_url( $props['url'] )
 				),
 				$post->ID
 			);
